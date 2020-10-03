64641de7582f5203bc29eebc6722989e5d42ec1b
Matheus Araujo <matheusnewff@gmail.com>
2020-10-03 15:29:14 -0300

    Criando operações de Instanciação - Categorias

--- src/main/java/com/matheusaraujo/estudoDeCaso/EstudoDeCasoApplication.java
+++ src/main/java/com/matheusaraujo/estudoDeCaso/EstudoDeCasoApplication.java
@@ -1,13 +1,41 @@
 package com.matheusaraujo.estudoDeCaso;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.matheusaraujo.estudoDeCaso.domain.Categoria;
+import com.matheusaraujo.estudoDeCaso.repository.CategoriaRepository;
+
+
 @SpringBootApplication
-public class EstudoDeCasoApplication {
+public class EstudoDeCasoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoDeCasoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		//Populando o banco de dados
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório ");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.addAll(Arrays.asList(cat1, cat2));
+		
+		categoriaRepository.saveAll(categorias);
+		
+		
 	}
 
 }
--- src/main/java/com/matheusaraujo/estudoDeCaso/resources/CategoriaResource.java
+++ src/main/java/com/matheusaraujo/estudoDeCaso/resources/CategoriaResource.java
@@ -1,15 +1,11 @@
 package com.matheusaraujo.estudoDeCaso.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.matheusaraujo.estudoDeCaso.domain.Categoria;
 import com.matheusaraujo.estudoDeCaso.service.CategoriaService;
 
