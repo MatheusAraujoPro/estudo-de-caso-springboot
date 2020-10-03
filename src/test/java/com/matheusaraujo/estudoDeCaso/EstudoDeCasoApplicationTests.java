862b7e1d358d7ae3e1f3e979156d4b1a7690ccfd
Matheus Araujo <matheusnewff@gmail.com>
2020-10-03 13:27:54 -0300

    Projeto Criado

---
+++ src/test/java/com/matheusaraujo/estudoDeCaso/EstudoDeCasoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusaraujo.estudoDeCaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoDeCasoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
