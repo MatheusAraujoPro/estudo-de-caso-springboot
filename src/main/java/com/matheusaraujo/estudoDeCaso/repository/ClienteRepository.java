b5c76cb22ba0ca2b5c001f411bb439f55e87865b
Matheus Araujo <matheusnewff@gmail.com>
2020-11-16 20:57:25 -0300

    Validacao customizadsa: email não repetido na inserção de cliente

--- src/main/java/com/matheusaraujo/estudoDeCaso/repository/ClienteRepository.java
+++ src/main/java/com/matheusaraujo/estudoDeCaso/repository/ClienteRepository.java
@@ -2,10 +2,15 @@ package com.matheusaraujo.estudoDeCaso.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.matheusaraujo.estudoDeCaso.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	//Usando padrões de nomes
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/matheusaraujo/estudoDeCaso/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/matheusaraujo/estudoDeCaso/service/validation/ClienteInsertValidator.java
@@ -2,14 +2,24 @@ package com.matheusaraujo.estudoDeCaso.service.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.matheusaraujo.estudoDeCaso.DTO.ClienteNewDTO;
+import com.matheusaraujo.estudoDeCaso.domain.Cliente;
 import com.matheusaraujo.estudoDeCaso.domain.enuns.TipoCliente;
+import com.matheusaraujo.estudoDeCaso.repository.ClienteRepository;
 import com.matheusaraujo.estudoDeCaso.resources.exception.FieldMessage;
 import com.matheusaraujo.estudoDeCaso.service.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -31,6 +41,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOucnpj", "CNPJ inválido"));
 			
 		}
+		
+		Cliente cliente = clienteRepository.findByEmail(objDto.getEmail());
+		
+		if(cliente != null)
+			list.add(new FieldMessage("email", "E-mail já existe na base de dados"));
 
 
 		// inclua os testes aqui, inserindo erros na lista
