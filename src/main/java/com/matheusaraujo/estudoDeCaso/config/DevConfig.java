7316b30cf6d25c95769e91decb3b0b1f498ee1e3
Matheus Araujo <matheusnewff@gmail.com>
2020-12-26 18:30:14 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/matheusaraujo/estudoDeCaso/config/DevConfig.java
+++ src/main/java/com/matheusaraujo/estudoDeCaso/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.matheusaraujo.estudoDeCaso.service.DbService;
+import com.matheusaraujo.estudoDeCaso.service.EmailService;
+import com.matheusaraujo.estudoDeCaso.service.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -27,6 +29,11 @@ public class DevConfig {
 		
 		dbService.instatiateDataBaseTest();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 	
 
